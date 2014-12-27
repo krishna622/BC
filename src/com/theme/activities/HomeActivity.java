c9a80d7670ad3c0e478604401535a90b530cf86a
raginilal <raginilal@192.168.1.7>
2014-12-27 19:41:28 +0530

    Commiting

--- src/com/theme/activities/HomeActivity.java
+++ src/com/theme/activities/HomeActivity.java
@@ -1,25 +1,42 @@
 package com.theme.activities;
 
 import com.theme.R;
-import com.theme.adapter.MyPagerAdapter;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 
-public class HomeActivity extends FragmentActivity {
+public class HomeActivity extends Activity {
 
-	private ViewPager pager;
-	private MyPagerAdapter adapter;
+	private ViewPager viewPager;
+	private ViewPagerAdapter adapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home);
 		
-		pager = (ViewPager) findViewById(R.id.viewPager);
-		adapter = new MyPagerAdapter(getSupportFragmentManager());
-		
-		pager.setAdapter(adapter);
+		viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+		adapter = new ViewPagerAdapter(HomeActivity.this);
+		viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+			@Override
+			public void onPageScrolled(int i, float v, int i2) {
+
+			}
+
+			@Override
+			public void onPageSelected(int arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int i) {
+
+			}
+		});
+		viewPager.setAdapter(adapter);
+
 	}
 }
